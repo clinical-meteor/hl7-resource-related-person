fc83419de9d03c51baa502cdfc8109345f024437
Abigail Watson <abigailwatson@Abigails-Mac-mini-377974.local>
2018-07-09 01:29:27 -0500

    package deps

--- package.js
+++ package.js
@@ -12,6 +12,7 @@ Package.onUse(function (api) {
   api.use('meteor-platform');
   api.use('mongo');
   api.use('aldeed:simple-schema@1.3.3');
+  api.use('aldeed:collection2@2.5.0');
   api.use('simple:json-routes@2.1.0');
   api.use('momentjs:moment@2.17.1');
   api.use('ecmascript@0.9.0');
@@ -19,16 +20,14 @@ Package.onUse(function (api) {
   api.use('http');
   api.use('react-meteor-data@0.2.15');
 
-  api.use('clinical:glass-ui@2.2.1');
+  api.use('clinical:glass-ui@2.1.4');
   api.use('clinical:extended-api@2.2.2');
   api.use('clinical:base-model@1.3.5');
   api.use('clinical:user-model@1.5.0');
-  api.use('clinical:hl7-resource-datatypes@3.0.0');
+  api.use('clinical:hl7-resource-datatypes@3.0.1');
   api.use('clinical:hl7-resource-bundle@1.3.10');
   api.use('matb33:collection-hooks@0.7.15');
   api.use('clinical:alert@2.1.3');
-  
-  api.imply('clinical:user-model');
 
   api.addFiles('lib/RelatedPersons.js', ['client', 'server']);
 
